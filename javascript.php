e5d950d783b1cf35262a35a69e29b6d0bf4756c7
André de Paula Terceiro <andreterceiro@yahoo.com.br>
2020-06-02 01:51:37 -0300

    Adição do type em algumas chamadas de script

--- javascript.php
+++ javascript.php
@@ -1,23 +1,23 @@
 	<!-- jQuery -->
-	<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
+	<script  type="text/javascript" src="http://code.jquery.com/jquery-1.10.1.min.js"></script>
 	
 	<script type="text/javascript" src="js/roundabout.js"></script>
 	<script type="text/javascript" src="js/scripts.js"></script>
 
 	<!-- jQuery Easing -->
-	<script src="js/jquery.easing.1.3.js"></script>
+	<script  type="text/javascript" src="js/jquery.easing.1.3.js"></script>
 	<!-- Bootstrap -->
-	<script src="js/bootstrap.min.js"></script>
+	<script  type="text/javascript" src="js/bootstrap.min.js"></script>
 	<!-- Waypoints -->
-	<script src="js/jquery.waypoints.min.js"></script>
+	<script  type="text/javascript" src="js/jquery.waypoints.min.js"></script>
 	<!-- Stellar -->
-	<script src="js/jquery.stellar.min.js"></script>
+	<script  type="text/javascript" src="js/jquery.stellar.min.js"></script>
 	<!-- Superfish -->
-	<script src="js/hoverIntent.js"></script>
-	<script src="js/superfish.js"></script>
+	<script  type="text/javascript" src="js/hoverIntent.js"></script>
+	<script  type="text/javascript" src="js/superfish.js"></script>
 
 	<!-- Main JS (Do not remove) -->
-	<script src="js/main.js"></script>
+	<script  type="text/javascript" src="js/main.js"></script>
 
 	<script>
 		$(function() {
