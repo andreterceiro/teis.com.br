c4fcd8fe25b38e1048300d25cd2cad3aa49b0519
André de Paula Terceiro <andre.terceiro@riachuelo.com.br>
2021-02-21 12:19:04 -0300

    feat: adjusting the price

--- index.php
+++ index.php
@@ -236,8 +236,8 @@
 					<div class="col-md-7 animate-box">
 						<div class="price-box animate-box">
 							<h2 class="pricing-plan">1 modalidade</h2>
-							<div class="price"><sup class="currency">$</sup>130,00<small>/mês</small></div>
-							<p>Neste plano você escolhe 1 modalidade. Muay Thai e Jiu-Jitsu por R$99,99 por mês. De R$720,00 por R$599,00 no plano semestral.</p>
+							<div class="price"><sup class="currency">$</sup>140,00<small>/mês</small></div>
+							<p>Neste plano você escolhe 1 modalidade. Muay Thai por R$99,99 por mês. De R$840,00 por R$599,00 no plano semestral.</p>
 						</div>
 					</div>
 
